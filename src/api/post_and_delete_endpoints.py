83efd5d12f800214c3cc11f77da9f174d56fccf9
Dillon Rego <dillonjrego17@gmail.com>
2023-05-18 22:08:39 -0700

    Final fixes for post and delete endpoints

--- src/api/post_and_delete_endpoints.py
+++ src/api/post_and_delete_endpoints.py
@@ -22,41 +22,42 @@ def add_entry(drug_id: int, drug_year: drug_yearJson):
   earliest recorded year not already filled. If a year is given it will 
   only fill in the drug data for the given year, (all inputs after tot_mftr) 
   """
-  post_result = []
-  json = []
  
   insertStmnt = """
   insert into drug_year (year, drug_id, total_dosage_units, total_claims, 
-  avg_spending_per_claim, avg_spending_per_dosage_weighted, outlier)
+  avg_spending_per_claim, avg_spending_per_dosage_weighted, total_spending, outlier)
   values ( :year, :drug_id, :total_dosage_units, :total_claims,
-  :avg_spending_per_claim, :avg_spending_per_dosage_weighted, :outlier)
+  :avg_spending_per_claim, :avg_spending_per_dosage_weighted, :total_spending, :outlier)
   on conflict (year, drug_id)
   do update set
     total_dosage_units = :total_dosage_units,
     total_claims = :total_claims,
     avg_spending_per_claim = :avg_spending_per_claim,
     avg_spending_per_dosage_weighted = :avg_spending_per_dosage_weighted,
+    total_spending = :total_spending,
     outlier = :outlier;
   """
   with db.engine.connect() as conn:
     try:
-      result = conn.execute(sqlalchemy.text(insertStmnt), 
-      [{"year" : drug_year.year, "drug_id" : drug_id, 
+      binds = {"year" : drug_year.year, "drug_id" : drug_id, 
       "total_dosage_units" : drug_year.total_dosage_units, 
       "total_claims" : drug_year.total_claims,
       "avg_spending_per_claim" : drug_year.avg_spending_per_claim, 
       "avg_spending_per_dosage_weighted" : drug_year.avg_spending_per_dosage_weighted,
       "total_spending" : drug_year.total_spending, 
-      "outlier" : drug_year.outlier}])
-      if result == None:
+      "outlier" : drug_year.outlier}
+      temp = sqlalchemy.text(insertStmnt).bindparams(**binds)
+      result = conn.execute(temp)
+      if result.rowcount != 0:
         print("Insert successful")
+        conn.commit()
       else:
         print("Insert failed")
     except Exception as e:
       conn.rollback()
       print("An Error has occurred", str(e))
 
-@router.post("/delete_entry/{drug_id}", tags=["entry"])
+@router.delete("/delete_entry/{drug_id}", tags=["entry"])
 def delete_entry(
   year: int = -1,
   drug_id: int = -1):
@@ -75,15 +76,16 @@ def delete_entry(
     delete from drug_year
     where drug_id = :d and year = :y"""
 
-  json = []
   with db.engine.connect() as conn:
     try:
       result = conn.execute(sqlalchemy.text(sql), [{"d":drug_id, "y": year}])
-      if result.status_code == 200:
+      if result.rowcount != 0:
         print("Delete successful")
+        conn.commit()
       else:
         print("Delete failed")
+        conn.rollback()
+        raise HTTPException(status_code=404, detail="drug not found")
     except Exception as e:
       conn.rollback()
       print("An Error has occurred", str(e))
-
