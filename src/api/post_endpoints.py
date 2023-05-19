8795548a54416475f281fb0c325e967b64ad3944
Dillon Rego <dillonjrego17@gmail.com>
2023-05-18 19:54:41 -0700

    Fixed Post endpoints

--- src/api/post_endpoints.py
+++ src/api/post_endpoints.py
@@ -22,41 +22,39 @@ def add_entry(drug_id: int, drug_year: drug_yearJson):
   earliest recorded year not already filled. If a year is given it will 
   only fill in the drug data for the given year, (all inputs after tot_mftr) 
   """
-  sql = """ 
-  select min(year)
-  from drug_year
-  where drug_id = :x"""
-
   post_result = []
   json = []
-  with db.engine.connect() as conn:
-    result = conn.execute(sqlalchemy.text(sql), [{"x":drug_id}])
-    if result.rowcount == 0:
-      raise HTTPException(status_code=404, detail="drug not found")
-
+ 
   insertStmnt = """
   insert into drug_year (year, drug_id, total_dosage_units, total_claims, 
   avg_spending_per_claim, avg_spending_per_dosage_weighted, outlier)
   values ( :year, :drug_id, :total_dosage_units, :total_claims,
   :avg_spending_per_claim, :avg_spending_per_dosage_weighted, :outlier)
+  on conflict (year, drug_id)
+  do update set
+    total_dosage_units = :total_dosage_units,
+    total_claims = :total_claims,
+    avg_spending_per_claim = :avg_spending_per_claim,
+    avg_spending_per_dosage_weighted = :avg_spending_per_dosage_weighted,
+    outlier = :outlier
   """
-
   with db.engine.connect() as conn:
-    post_result = conn.execute(sqlalchemy.text(insertStmnt), 
-      [{"year" : drug_year.year}, {"drug_id" : drug_id}, 
-      {"total_dosage_units" : drug_year.total_dosage_units}, 
-      {"total_claims" : drug_year.total_claims},
-      {"avg_spending_per_claim" : drug_year.avg_spending_per_claim}, 
-      {"avg_spending_per_dosage_weighted" : drug_year.avg_spending_per_dosage_weighted},
-      {"total_spending" : drug_year.total_spending}, 
-      {"outlier" : drug_year.outlier}])
-    if post_result.rowcount == 0:
-      raise HTTPException(status_code=404, detail="drug not found")
-    for row in post_result:
-      json.append({
-        'rows': row.post_result
-      })
-  return json
+    try:
+      result = conn.execute(sqlalchemy.text(insertStmnt), 
+      [{"year" : drug_year.year, "drug_id" : drug_id, 
+      "total_dosage_units" : drug_year.total_dosage_units, 
+      "total_claims" : drug_year.total_claims,
+      "avg_spending_per_claim" : drug_year.avg_spending_per_claim, 
+      "avg_spending_per_dosage_weighted" : drug_year.avg_spending_per_dosage_weighted,
+      "total_spending" : drug_year.total_spending, 
+      "outlier" : drug_year.outlier}])
+      if result.rowcount == 1:
+        print("Insert successful")
+      else:
+        print("Insert failed")
+    except Exception as e:
+      conn.rollback()
+      print("An Error has occurred", str(e))
 
 @router.post("/delete_entry/{drug_id}", tags=["entry"])
 def delete_entry(
@@ -67,27 +65,25 @@ def delete_entry(
   it will automatically delete all for that entry,
   if a year is given it will only delete information for that calendar year.
   """
-  first_sql = """select drug_id from drug_year where drug_id = {}""", drug_id
-  with db.engine.connect() as conn:
-    test_result = conn.execute(sqlalchemy.text(first_sql))
-    if test_result.rowcount == 0:
-      raise HTTPException(status_code=404, detail="invalid drug_id")
-
-  sql = """
-  delete from drug_year
-  where drug_id = :y"""
-
+  
   if year == -1:
-    additional_string = ""
+    sql = """
+    delete from drug_year
+    where drug_id = :d"""
   else:
-    additional_string = " and year = {}", year 
+        sql = """
+    delete from drug_year
+    where drug_id = :d and year = :y"""
+
   json = []
   with db.engine.connect() as conn:
-    result = conn.execute(sqlalchemy.text(sql), [{"y":additional_string}])
-    if result.rowcount == 0:
-      raise HTTPException(status_code=404, detail="drug not found")
-    for row in result:
-      json.append({
-        'rows': row.post_result
-      })
-  return json
+    try:
+      result = conn.execute(sqlalchemy.text(sql), [{"d":drug_id, "y": year}])
+      if result.rowcount == 1:
+        print("Delete successful")
+      else:
+        print("Delete failed")
+    except Exception as e:
+      conn.rollback()
+      print("An Error has occurred", str(e))
+
